cbd48c97602b325170eae080f7691a3c9550fe68
RudyHarun99 <rdyhrn@gmail.com>
2024-08-14 23:33:32 +0700

    progress 1: all numbers

--- 1.chainable-counter-object/index.js
+++ 1.chainable-counter-object/index.js
@@ -2,7 +2,25 @@
 
 // start coding here
 // --------
-
+const counter = {
+  count: 0,
+  increment() {
+    this.count++;
+    return this;
+  },
+  decrement() {
+    this.count--;
+    return this;
+  },
+  reset() {
+    this.count = 0;
+    return this;
+  },
+  showCount() {
+    console.log(this.count);
+    return this;
+  },
+}
 // --------
 // the end of your code
 counter.increment().increment().decrement().showCount().reset().showCount();
--- 2.find-items/index.js
+++ 2.find-items/index.js
@@ -9,6 +9,15 @@ const result = findItems(arrayOfObjects, function (item) {
   return item.id < 3;
 });
 
+console.log(result);
 // Read the README.md file first
 // Expected output [ { id: 1, name: 'Alice' }, { id: 2, name: 'Bob' } ]
 // Make the function findItems as a function declaration here
+function findItems(array, callback) {
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+    if (callback(el)) output.push(el);
+  }
+  return output;
+}
