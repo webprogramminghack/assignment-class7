ee9ab46112ce57896b7a1c9d65012aff6ec2f08a
Dion <prep.prodigy1@gmail.com>
2024-08-16 14:51:35 +0700

    Finishing Assigment #7, JS Object & Array

--- 1.chainable-counter-object/index.js
+++ 1.chainable-counter-object/index.js
@@ -2,7 +2,69 @@
 
 // start coding here
 // --------
+//1. Create the `counter` Object
+{
+  let counter = {
+    //A property that holds the current count (initialize it to 0).
+    count: 0,
+    //A method that increments the `count` by 1.
+    increment() {
+      this.count++;
+    },
+    //A method that decrements the `count` by 1.
+    decrement() {
+      this.count--;
+    },
+    //A method that resets the `count` to 0.
+    reset() {
+      this.count = 0;
+    },
+    //A method that displays the current count
+    showCount() {
+      console.log(this.count);
+    },
+  };
+
+  counter.increment();
+  counter.decrement();
+  counter.reset();
+  //counter.showCount();
+}
+
+//2. Modify the Methods for Chaining
+{
+  let counter = {
+    //A property that holds the current count (initialize it to 0).
+    count: 0,
+    //A method that increments the `count` by 1.
+    increment() {
+      this.count++;
+      return this;
+    },
+    //A method that decrements the `count` by 1.
+    decrement() {
+      this.count--;
+      return this;
+    },
+    //A method that resets the `count` to 0.
+    reset() {
+      this.count = 0;
+      return this;
+    },
+    //A method that displays the current count
+    showCount() {
+      console.log(this.count);
+      return this;
+    },
+  };
+
+  // counter.increment();
+  // counter.decrement();
+  // counter.reset();
+  // counter.showCount();
+
+  counter.increment().increment().decrement().showCount().reset().showCount();
+}
 
 // --------
 // the end of your code
-counter.increment().increment().decrement().showCount().reset().showCount();
--- 2.find-items/index.js
+++ 2.find-items/index.js
@@ -9,6 +9,17 @@ const result = findItems(arrayOfObjects, function (item) {
   return item.id < 3;
 });
 
-// Read the README.md file first
-// Expected output [ { id: 1, name: 'Alice' }, { id: 2, name: 'Bob' } ]
-// Make the function findItems as a function declaration here
+//**Create the `findItems` Function:**
+function findItems(array, callback) {
+  const result = [];
+
+  //The `findItems` function should iterate over the `array` and apply the `callback` function to each element.
+  for (let i = 0; i < array.length; i++) {
+    //It should return a new array containing all elements for which the `callback` returns `true`.
+    if (callback(array[i])) {
+      result.push(array[i]);
+    }
+  }
+
+  console.log(result);
+}
