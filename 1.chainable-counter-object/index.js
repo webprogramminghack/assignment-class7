14badf867d9b939ca8edfebae536e47261fb51ee
iskhakmuhamad <iskhakmuhama@gmail.com>
2024-08-20 19:18:01 +0700

    finish assignment 7

--- 1.chainable-counter-object/index.js
+++ 1.chainable-counter-object/index.js
@@ -2,7 +2,29 @@
 
 // start coding here
 // --------
+const counter = {
+  total: 0,
 
+  increment() {
+    this.total += 1;
+    return this;
+  },
+
+  decrement() {
+    this.total -= 1;
+    return this;
+  },
+
+  reset() {
+    this.total = 0;
+    return this;
+  },
+
+  showCount() {
+    console.log(this.total);
+    return this;
+  }
+};
 // --------
 // the end of your code
 counter.increment().increment().decrement().showCount().reset().showCount();
