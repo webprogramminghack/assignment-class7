89c3eb4f20d24c00bf63974ebe6cb527f673bbee
alfatfebry <febryalfat@gmail.com>
2024-08-22 17:37:00 +0700

    Assignment class 7

--- 2.find-items/index.js
+++ 2.find-items/index.js
@@ -5,10 +5,23 @@ const arrayOfObjects = [
   { id: 4, name: 'David' },
 ];
 
-const result = findItems(arrayOfObjects, function (item) {
-  return item.id < 3;
+const result = findItems(arrayOfObjects, function (key) {
+  return key.id < 3;
 });
 
+function findItems(array, callback) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(result);
 // Read the README.md file first
 // Expected output [ { id: 1, name: 'Alice' }, { id: 2, name: 'Bob' } ]
 // Make the function findItems as a function declaration here
